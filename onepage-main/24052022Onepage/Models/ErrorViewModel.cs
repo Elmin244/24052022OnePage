45ac485f71964d3fc4fff0a9cf516717e80583c9
unknown <tu7d5w0m5@code.edu.az>
2022-06-24 14:32:59 -0800

    first commit

---
+++ onepage-main/24052022Onepage/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace 24052022Onepage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
